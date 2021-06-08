c1263d6a424a4b56f2b81a3ae25af631ed3b63b1
Faisal Riyaz <faisal@deepsource.io>
2021-06-08 20:52:28 +0530

    Add PTC-W0052, PTC-W0046, PTC-W6002

---
+++ miscellaneous.py
@@ -0,0 +1,12 @@
+class Orange:
+    '''We mean, the fruit.'''
+    orange = "#FFA500"
+    
+    # Other class implementations
+    
+    def get_orange(self):
+        return abc
+
+fruit = Orange()
+render_to_frontend(fruit.orange)  # Rendering a color, but one can get confused with the fruit
+render_bg(fruit.get_orange)
